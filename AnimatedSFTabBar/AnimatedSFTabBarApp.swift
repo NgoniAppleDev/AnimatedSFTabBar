d436d5aa7e7ceae50956197f71afefdd32348065
Ngoni  Developer <ngonikatsidzira@icloud.com>
2025-04-07 17:40:50 +0200

    Initial Commit

---
+++ AnimatedSFTabBar/AnimatedSFTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSFTabBarApp.swift
+//  AnimatedSFTabBar
+//
+//  Created by Ngoni Katsidzira  on 7/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSFTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
